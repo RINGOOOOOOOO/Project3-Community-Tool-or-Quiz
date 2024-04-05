1230ef5b2f67af175205df9bbd87587715d2e8d7
RINGOOOOOOOO <xuj678@newschool.edu>
2024-04-05 10:06:20 -0400

    update

--- page10.js
+++ page10.js
@@ -1,57 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#name").innerHTML = localStorage.getItem("name");
+  document.querySelector("#name").style.color = localStorage.getItem("color");
+});
+document.addEventListener("DOMContentLoaded", function () {
   const resultSpan = document.getElementById("result");
   const result2Span = document.getElementById("result2");
+  const result3Span = document.getElementById("result3");
   const submitButton = document.getElementById("submit");
-  const textId = document.getElementById("text");
-  const textId2 = document.getElementById("text2");
-  const options = document.querySelectorAll(".quiz-options li");
 
-  let countA = parseInt(localStorage.getItem("countA")) || 0;
-  let countB = parseInt(localStorage.getItem("countB")) || 0;
-  let countC = parseInt(localStorage.getItem("countC")) || 0;
   const Option05 = localStorage.getItem("Option05");
   const Option06 = localStorage.getItem("Option06");
   const Option07 = localStorage.getItem("Option07");
-  const Option08 = localStorage.getItem("Option08");
 
-  function setResultText(option) {
-    if (Option05 === "1. No") {
-      resultSpan.textContent = "test!";
-      // result2Span.textContent =
-      //   "Under normal circumstances, having a bowel movement three times a day or once every three days are both considered reasonable ranges. As long as the bowel movements are regular, and the shape and color of the stool are healthy, there is no cause for concern!";
-    } else if (option === "2. At least twice a day") {
-      resultSpan.textContent = "Wow, You have very healthy poop habits!";
-      result2Span.textContent =
-        "Under normal circumstances, having a bowel movement three times a day or once every three days are both considered reasonable ranges. As long as the bowel movements are regular, and the shape and color of the stool are healthy, there is no cause for concern!";
-    } else if (
-      option === "3. Once or twice a day" ||
-      option === "4. Every 2-3 days" ||
-      option === "9. I prefer not to answer"
-    ) {
-      textId.style.display = "none";
-      result2Span.textContent =
-        "Under normal circumstances, having a bowel movement three times a day or once every three days are both considered reasonable ranges. As long as the bowel movements are regular, and the shape and color of the stool are healthy, there is no cause for concern!";
-    } else if (
-      option === "5. Every 4-7 days" ||
-      option === "6. Every 2-3 weeks" ||
-      option === "7. Once a month or longer" ||
-      option === "8. I don't poop"
-    ) {
-      resultSpan.textContent = "OH NO, You may have constipation troubles!";
-      result2Span.textContent =
-        "Under normal circumstances, having a bowel movement three times a day or once every three days are both considered reasonable ranges. As long as the bowel movements are regular, and the shape and color of the stool are healthy, there is no cause for concern!";
-    } else {
-      textId.style.display = "none";
-      textId2.style.display = "none";
-    }
+  console.log("page07:", Option07);
+  console.log("page06:", Option06);
+  console.log("page05:", Option05);
+
+  if (Option05 === "2. No") {
+    resultSpan.textContent =
+      "You need to pay attention to healthy eating, nutrition balance, and fiber intake;";
   }
-
-  if (selectedOption) {
-    setResultText(selectedOption);
+  if (Option06 === "1. yeah") {
+    result2Span.textContent = "Relax your mind while 💩 and avoid anxiety;";
   }
-
-  if (!selectedOption) {
-    submitButton.style.display = "none";
+  if (Option07 === "3. 10-30min" || Option07 === "4. over 30min") {
+    result3Span.textContent = "It's best to keep 💩 time within 10 minutes;";
   }
 
   submitButton.addEventListener("click", function () {
